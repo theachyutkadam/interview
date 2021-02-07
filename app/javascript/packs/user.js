aa085878dcbebc032d58a55b2aba9df35e814c43
ak <achyut.kadam@definelabs.com>
2021-02-07 11:57:46 +0530

    javascript or jquery setup

--- app/javascript/packs/user.js
+++ app/javascript/packs/user.js
@@ -1,4 +1,4 @@
-alert("sdlfkjd");
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 $( document ).ready(function() {
   alert("sdlfkjd");
   var method_name = (window.location.pathname + '').toString().split('/').pop();
