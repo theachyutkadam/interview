d25ee7cefdeae6cef92d3423ed85844e5c7e64b3
ak <achyut.kadam@definelabs.com>
2021-02-07 11:36:52 +0530

    added collect cities method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,20 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :gender, :country_id, :state_id, :city_id, :phone, :email, :password, :current_password)}
   end
+
+  def collect_cities
+    if params[:state_id].present?
+      @cities = State.find(params[:state_id]).cities
+    else
+      @cities = City.all
+    end
+    if request.xhr?
+      respond_to do |format|
+        format.json {
+          render json: {cities: @cities}
+        }
+      end
+    end
+  end
+
 end
