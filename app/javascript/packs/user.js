ebf3eb3ce904f2f3b63bada22ed36190240b1a52
ak <achyut.kadam@definelabs.com>
2021-02-07 11:27:19 +0530

    find selected state cities using ajax calling

---
+++ app/javascript/packs/user.js
@@ -0,0 +1,32 @@
+alert("sdlfkjd");
+$( document ).ready(function() {
+  alert("sdlfkjd");
+  var method_name = (window.location.pathname + '').toString().split('/').pop();
+  (method_name == "edit") ? $("#user_state_id").prop("disabled", true) : $("#user_state_id").prop("disabled", false);
+  (method_name == "edit") ? $("#user_city_id").prop("disabled", true) : $("#user_city_id").prop("disabled", false);
+
+  $("#user_state_id").change(function(){
+    var state_id = $(this).val();
+    (state_id == '') ? $("#user_city_id").prop("disabled", true) : $("#user_city_id").prop("disabled", false);
+    $.ajax({
+      url: "/branches/collect_cities/",
+      method: "GET",
+      dataType: "json",
+      data: {state_id: state_id},
+      error: function (xhr, status, error) {
+        console.error('AJAX Error: ' + status + error);
+      },
+      success: function (response) {
+        var cities = response["cities"];
+        $("#user_city_id").empty();
+        $("#user_city_id").append('<option>Select City</option>');
+        for(var i = 0; i < cities.length; i++){
+          $("#user_city_id").append('<option value="' + cities[i]["id"] + '">' +cities[i]["name"] + '</option>');
+        }
+      }
+    });
+
+  });
+
+
+});
