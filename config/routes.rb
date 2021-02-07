483e87540d1f852051f90572bac94f830393730a
ak <achyut.kadam@definelabs.com>
2021-02-07 12:00:12 +0530

    comment find cities routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
   devise_for :users
-  get 'collect_cities'
+  # get 'collect_cities'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
